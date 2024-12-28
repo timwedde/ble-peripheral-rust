21447abb47d147a4e4fb256a51447d77b33f89aa
Rohit Sangwan <rohitsangwan647@gmail.com>
2024-12-28 13:58:56 +0530

    Fix Windows

--- src/peripheral/winrt/mod.rs
+++ src/peripheral/winrt/mod.rs
@@ -5,9 +5,11 @@ mod win_event_handler;
 mod win_utils;
 
 use self::peripheral_manager::PeripheralManager;
+use super::PeripheralImpl;
 use crate::error::Error;
 use crate::gatt::peripheral_event::PeripheralEvent;
 use crate::gatt::service::Service;
+use async_trait::async_trait;
 use tokio::sync::mpsc::Sender;
 use uuid::Uuid;
 
@@ -15,43 +17,46 @@ pub struct Peripheral {
     peripheral_manager: PeripheralManager,
 }
 
-impl Peripheral {
-    pub async fn new(sender_tx: Sender<PeripheralEvent>) -> Result<Self, Error> {
+#[async_trait]
+impl PeripheralImpl for Peripheral {
+    type Peripheral = Self;
+
+    async fn new(sender_tx: Sender<PeripheralEvent>) -> Result<Self, Error> {
         Ok(Self {
             peripheral_manager: PeripheralManager::new(sender_tx).await,
         })
     }
 
-    pub async fn is_powered(&self) -> Result<bool, Error> {
+    async fn is_powered(&mut self) -> Result<bool, Error> {
         Ok(self.peripheral_manager.is_powered().await?)
     }
 
-    pub async fn start_advertising(&self, name: &str, uuids: &[Uuid]) -> Result<(), Error> {
+    async fn start_advertising(&mut self, name: &str, uuids: &[Uuid]) -> Result<(), Error> {
         if let Err(err) = self.peripheral_manager.start_advertising(name, uuids).await {
             return Err(Error::from(err));
         }
         Ok(())
     }
 
-    pub async fn stop_advertising(&self) -> Result<(), Error> {
+    async fn stop_advertising(&mut self) -> Result<(), Error> {
         if let Err(err) = self.peripheral_manager.stop_advertising().await {
             return Err(Error::from(err));
         }
         Ok(())
     }
 
-    pub async fn is_advertising(&self) -> Result<bool, Error> {
+    async fn is_advertising(&mut self) -> Result<bool, Error> {
         Ok(self.peripheral_manager.is_advertising().await?)
     }
 
-    pub async fn add_service(&mut self, service: &Service) -> Result<(), Error> {
+    async fn add_service(&mut self, service: &Service) -> Result<(), Error> {
         if let Err(err) = self.peripheral_manager.add_service(service).await {
             return Err(Error::from(err));
         }
         Ok(())
     }
 
-    pub async fn update_characteristic(
+    async fn update_characteristic(
         &mut self,
         characteristic: Uuid,
         value: Vec<u8>,

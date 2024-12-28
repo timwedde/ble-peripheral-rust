1c4f11bffa23ce8a904e4b694565d38da4f88a95
rohitsangwan01 <rohitsangwan647@gmail.com>
2024-12-28 15:02:24 +0530

    Fix Linux

--- src/peripheral/bluez/bluez_utils.rs
+++ src/peripheral/bluez/bluez_utils.rs
@@ -1,9 +1,15 @@
+use crate::error::{self, Error, ErrorType};
 use bluer::gatt::local::CharacteristicControl;
 use uuid::Uuid;
-
 #[derive(Debug)]
 pub(crate) struct CharNotifyHandler {
     pub service_uuid: Uuid,
     pub characteristic_uuid: Uuid,
     pub control: CharacteristicControl,
+}
+
+impl From<bluer::Error> for error::Error {
+    fn from(error: bluer::Error) -> Self {
+        Error::from_string(error.to_string(), ErrorType::Bluez)
+    }
 }
